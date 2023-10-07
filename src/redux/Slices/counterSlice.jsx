6655c025b0005504c30b448560ae4884fae8c1e3
pratikw007 <waghmarepratik287@gmail.com>
2023-10-07 13:16:46 +0530

    Shopping app by react

---
+++ src/redux/Slices/counterSlice.jsx
@@ -0,0 +1,35 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+//  const initialState = {
+//     value : 0,
+//  }
+
+// export const CounterSlice =  createSlice({
+//     name: "counter",
+//     initialState,
+//     reducers : {  // set functionality--> main logic likhna hai yaha pr
+//         // Increment :
+//         increment : (state) =>{
+//             state.value += 1;
+//         },
+//         // decrement logic
+//         decrement : (state) => {
+//             state.value -= 1;
+//         }
+//     }
+// })
+
+
+// export const {increment,decrement} = CounterSlice.actions;
+// export default CounterSlice.reducer;
+
+
+import React from 'react'
+
+const counterSlice = () => {
+  return (
+    <div>counterSlice</div>
+  )
+}
+
+export default counterSlice
